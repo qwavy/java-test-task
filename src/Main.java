e362ae9b66504732470492ee5840f7f6e6800624
qwavy <nursultanr585@gmail.com>
2025-07-20 19:21:39 +0500

    feat: adding to types to files

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try{
+            File strings = new File("strings.txt");
+            File integers = new File("integers.txt");
+            File floats = new File("floats.txt");
+            FileWriter stringsWriter = new FileWriter("strings.txt");
+            FileWriter integersWriter = new FileWriter("integers.txt");
+            FileWriter floatsWriter = new FileWriter("floats.txt");
+
             BufferedReader reader = new BufferedReader(new FileReader("filename.txt"));
             String line = reader.readLine();
 
             while (line != null){
                 if(isNumber(line)){
+                    integersWriter.write(line + "\n");
                     System.out.println("number " + line);
                 }else if(isFloat(line)){
+                    floatsWriter.write(line + "\n");
                     System.out.println("float " + line);
                 }else {
+                    stringsWriter.write(line + "\n");
                     System.out.println(line);
                 }
                 line = reader.readLine();
             }
 
+            integersWriter.close();
+            floatsWriter.close();
+            stringsWriter.close();
             reader.close();
         } catch (IOException e){
+            System.out.println(e);
             e.printStackTrace();
         }
     }
+
     public static boolean isNumber(String arg) {
         try{
             Integer.parseInt(arg);
@@ -39,4 +54,5 @@ public class Main {
             return  false;
         }
     }
+
 }
