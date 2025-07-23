133a60540b9ec1cda413090e771d8ac215c436e7
Nursultan <97668069+qwavy@users.noreply.github.com>
2025-07-24 01:29:09 +0500

    feat: dont create if no data got

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ import java.util.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            List<String> inputFiles = Arrays.stream(args)
+                    .filter(a -> a.endsWith(".txt"))
+                    .collect(Collectors.toList());
+
+            if(inputFiles.isEmpty()){
+                System.out.println("no input files found");
+            }
+
             boolean minStats = Arrays.asList(args).contains("-s");
             boolean maxStats = Arrays.asList(args).contains("-f");
             boolean appendToFile = Arrays.asList(args).contains("-a");
@@ -17,32 +26,64 @@ public class Main {
 
             FileNames fileNames = new FileNames(args);
 
-            FileWriter stringsWriter = new FileWriter(fileNames.getPath() + "strings.txt", appendToFile);
-            FileWriter integersWriter = new FileWriter(fileNames.getPath() + "integers.txt", appendToFile);
-            FileWriter floatsWriter = new FileWriter(fileNames.getPath() + "floats.txt", appendToFile);
+            FileWriter stringsWriter = null;
+            FileWriter integersWriter = null;
+            FileWriter floatsWriter = null;
 
-            BufferedReader reader = new BufferedReader(new FileReader("filename.txt"));
-            String line = reader.readLine();
 
-            while (line != null) {
-                if (isNumber(line)) {
-                    integersWriter.write(line + "\n");
-                    integerStats.add(Integer.parseInt(line));
-                } else if (isFloat(line)) {
-                    floatsWriter.write(line + "\n");
-                    floatStats.add(Float.parseFloat(line));
-                } else {
-                    stringsWriter.write(line + "\n");
-                    stringStats.add(line);
-                }
-                line = reader.readLine();
 
+            List<BufferedReader> readers = new ArrayList<>();
+            for (String name : inputFiles) {
+                readers.add(new BufferedReader(new FileReader(name)));
             }
 
-            integersWriter.close();
-            floatsWriter.close();
-            stringsWriter.close();
-            reader.close();
+            boolean anyLeft = true;
+            while (anyLeft) {
+                anyLeft = false;
+
+                for (int i = 0; i < readers.size(); i++) {
+                    BufferedReader br = readers.get(i);
+                    if (br == null) continue;
+
+                    String line = br.readLine();
+                    if (line != null) {
+                        anyLeft = true;
+                        if (isNumber(line)) {
+                            if(integersWriter == null){
+                                integersWriter = new FileWriter(fileNames.getPath() + "integers.txt", appendToFile);
+                            }
+                            integersWriter.write(line + "\n");
+                            integerStats.add(Integer.parseInt(line));
+                        } else if (isFloat(line)) {
+                            if(floatsWriter == null){
+                                floatsWriter = new FileWriter(fileNames.getPath() + "floats.txt", appendToFile);
+                            }
+                            floatsWriter.write(line + "\n");
+                            floatStats.add(Float.parseFloat(line));
+                        } else {
+                            if(stringsWriter == null){
+                                stringsWriter = new FileWriter(fileNames.getPath() + "strings.txt", appendToFile);
+                            }
+                            stringsWriter.write(line + "\n");
+                            stringStats.add(line);
+                        }
+                    } else {
+                        br.close();
+                        readers.set(i, null);
+                    }
+                }
+            }
+
+            if(integersWriter != null){
+                integersWriter.close();
+            }
+            if(floatsWriter != null){
+                floatsWriter.close();
+            }
+            if(stringsWriter != null){
+                stringsWriter.close();
+            }
+
 
             Stats stats = new Stats(stringStats, integerStats, floatStats, maxStats, minStats);
         } catch (IOException e) {
